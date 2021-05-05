951d62bd28944d0b8ac73dc7c93ec7201c9721e9
Hebert324 <hebertrocha76@gmail.com>
2021-05-05 14:37:19 -0300

    Initial commit

---
+++ data.js
@@ -0,0 +1,21 @@
+export default [
+  {
+    title:
+      "Vida louca",
+    artist: "Mc Poze",
+    cover: "mcpozeimg.jpg",
+    file: "mcpoze-vidalouca.mp3"
+  },
+  {
+    title: "Preta do cabelo cacheado",
+    artist: "MC TH CDM",
+    cover: "mcthcdmimg.jpg",
+    file: "pretadoCabeloCacheado.mp3"
+  },
+  {
+    title: "Homenagem aos Relíguias 1.0",
+    artist: "DJ MATT-D",
+    cover: "djmattdimg.jpg",
+    file: "Djmattd-homenagem1.0.mp3"
+  }
+];
